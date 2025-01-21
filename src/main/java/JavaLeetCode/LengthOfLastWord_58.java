be5230fac59bbec5f758f68f865643e15d122e05
Ritika Joshi <me.ritikajoshi@gmail.com>
2025-01-21 21:04:35 +0545

    Solution for length of last word problem

---
+++ src/main/java/JavaLeetCode/LengthOfLastWord_58.java
@@ -0,0 +1,15 @@
+package JavaLeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LengthOfLastWord_58 {
+    public static void main(String[] args) {
+        String s="Hello World";
+        System.out.println(lengthOfLastWord(s));
+    }
+    public static int lengthOfLastWord(String s) {
+        String[] words =s.split(" ");
+        return words[words.length-1].length();
+    }
+}
